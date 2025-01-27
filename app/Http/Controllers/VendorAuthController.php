de1772d1dec501ea592ceade1b01c10d485cb22f
pembattech <pembat.tech@gmail.com>
2025-01-27 16:47:33 +0545

    Add unique vendor_uniqid generation during vendor registration

--- app/Http/Controllers/VendorAuthController.php
+++ app/Http/Controllers/VendorAuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Vendor;
+use App\Models\vendor_uniqid;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -23,13 +24,30 @@ class VendorAuthController extends Controller
             'location' => 'required|string|max:255',
         ]);
 
-        Vendor::create([
+        $create_vendor = Vendor::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'location' => $request->location,
 
         ]);
+
+        if ($create_vendor) {
+
+            $vendorName = $request->name;
+            $uniqueId = $vendorName;
+            $counter = 1;
+
+            while (vendor_uniqid::where('unique_id', $uniqueId)->exists()) {
+                $uniqueId = $vendorName . $counter;
+                $counter++;
+            }
+
+            vendor_uniqid::create([
+                'vendor_id' => $create_vendor->id,
+                'unique_id' => $uniqueId,
+            ]);
+        }
 
         return redirect()->route('vendor.login')->with('success', 'Registration successful! Please login.');
     }
